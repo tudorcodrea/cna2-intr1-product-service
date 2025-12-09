d1ad375a22712205dc51636d7c73ceeeab9644bd
Tudor <codrea_tudor@yahoo.com>
2025-12-09 10:47:45 +0200

    fixes event publishing

--- src/main/java/com/example/productservice/SnsService.java
+++ src/main/java/com/example/productservice/SnsService.java
@@ -2,11 +2,14 @@ package com.example.productservice;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
 import software.amazon.awssdk.services.sns.model.PublishResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 @Service
 public class SnsService {
@@ -14,12 +17,13 @@ public class SnsService {
     private final SnsClient snsClient;
     private final ObjectMapper objectMapper;
 
-    @Value("${sns.topic.arn}")
+    @Value("${sns.topic.name}")
     private String topicArn;
 
     public SnsService() {
         this.snsClient = SnsClient.create();
         this.objectMapper = new ObjectMapper();
+        this.objectMapper.registerModule(new JavaTimeModule());
     }
 
     public void publishProductEvent(Product product) {
